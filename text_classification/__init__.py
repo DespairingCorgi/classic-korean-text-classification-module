644bade58631e27a1f684c0f1aea839f5c86178a
Jooyoung Jeong <jooyoungworknd@gmail.com>
2023-10-20 15:01:44 +0900

    saperated methods

--- text_classification/__init__.py
+++ text_classification/__init__.py
@@ -11,10 +11,18 @@ import xgboost as xgb
 tokenizer = Okt()
 
 fasttext_model = None
-cur_model = None
+cur_fasttext_model = None
 
 tf_vec = ['tf', 'tfidfvectorizer', 'tfvectorizer']
 ft_vec = ['ft', 'fasttext', 'fast_text']
+
+def report_result(method, labels, predictions):
+    print("="*100)
+    print(f"Method: {method}")
+    print(classification_report(labels, predictions))
+    print("\nconfusion matrix:")
+    print(confusion_matrix(labels, predictions), "\n")
+    print("="*100)
 
 def test_model(model, x_test, y_test, **kwargs):
     
@@ -39,21 +47,53 @@ def test_model(model, x_test, y_test, **kwargs):
     else:
         pred = model.predict(x_test)
     
-    print("="*100)
-    print(f"Method: {method}")
-    print(classification_report(y_test, pred))
-    print("\nconfusion matrix:")
-    print(confusion_matrix(y_test, pred), "\n")
-    print("="*100)
-            
+    report_result(method, y_test, pred)        
+
+
+def tf_vectorize(texts, **kwargs):
+    if "vectorizer_model" in kwargs.keys():
+        return kwargs["vectorizer_model"].transform(texts)
+    else:
+        vectorizer = TfidfVectorizer()
+        return vectorizer.fit_transform(texts)
+
+def tf_vectorize_sets(*textsets, **kwargs):
+    if "vectorizer_model" in kwargs.keys():
+        vectorizer = kwargs["vectorizer_model"]
+    else:
+        vectorizer = TfidfVectorizer()
+    
+    return vectorizer, [vectorizer.fit_transform(texts) if i == 0 \
+        else vectorizer.transform(texts) \
+        for i, texts in enumerate(textsets)]
+
+def fasttext_vectorize_sets(*textsets, **kwargs):
+    '''
+        model (str) model file name or None(for default model)
+    '''
+    global fasttext_model
+    global cur_model
+    
+    vectorizer=None
+    if "model" in kwargs.keys():
+        vectorizer = kwargs["model"]
+    
+    if fasttext_model == None:
+        if vectorizer == None:
+            fasttext_model = load_model()
+        else:
+            fasttext_model = load_model(model_name=vectorizer)
+    else:
+        if vectorizer != cur_fasttext_model:
+            fasttext_model = load_model(model_name=vectorizer)
+        cur_fasttext_model = vectorizer
+        
+    return vectorizer, [get_fasttext_vectors(fasttext_model, texts.values.tolist()) for texts in textsets]
+        
+
 
 def sklearn_ml(func):
     def wrapper(*args, **kwargs):
-        # for arg in args:
-        #     print(type(arg))
-        global fasttext_model
-        global cur_model
-        
         vector_mode = kwargs['vector_mode'] if 'vector_mode' in kwargs.keys() else 'tf'.lower()
         model = kwargs['model'] if 'model' in kwargs.keys() else None        
         
@@ -62,23 +102,10 @@ def sklearn_ml(func):
             vector_mode = 'tf'
         
         if vector_mode in tf_vec:
-            vectorizer = TfidfVectorizer()
-            train_vec = vectorizer.fit_transform(args[0])
-            test_vec = vectorizer.transform(args[1])        
+            vectorizer, (train_vec, test_vec) = tf_vectorize_sets(args[0], args[1], vectorizer_model=TfidfVectorizer())
+                
         elif vector_mode.lower() in ft_vec:
-            if fasttext_model == None:
-                print("load new model...")
-                if model == None:
-                    fasttext_model = load_model()
-                else:
-                    fasttext_model = load_model(model_name=model)
-            else:
-                if model != cur_model:
-                    fasttext_model = load_model(model_name=model)
-            cur_model = model
-            
-            train_vec = get_fasttext_vectors(fasttext_model, args[0].values.tolist())
-            test_vec = get_fasttext_vectors(fasttext_model, args[1].values.tolist())
+            vectorizer, (train_vec, test_vec) = fasttext_vectorize_sets(args[0], args[1])
         else:
             pass
         
@@ -86,41 +113,16 @@ def sklearn_ml(func):
         #print(test_vec.shape)
         
         model,method = func(train_vec, test_vec, args[2], args[3], **kwargs)
-        # if func.__name__ == "lightgbm_classification":
-        #     initial_preds = model.predict(test_vec, num_iteration=model.best_iteration)
-        #     if kwargs["num_class"] > 2:  # mult softmax
-        #         pred = initial_preds.argmax(axis=1) 
-        #         # [p.argmax() for p in initial_preds]
-        #     elif kwargs["num_class"] == 2: # logistic binary
-        #         pred = [1 if p >= 0.5 else 0 for p in initial_preds]
-        #     else:
-        #         raise Exception("num_class error: input must be integer over 1")
-        # elif func.__name__=="xgboost_classification":
-        #     d_test = xgb.DMatrix(test_vec, args[3])
-        #     initial_preds = model.predict(d_test)
-        #     if kwargs["num_class"] > 2: # mult softmax
-        #         pred = initial_preds.argmax(axis=1) 
-        #         # [p.argmax() for p in initial_preds]
-        #     elif kwargs["num_class"] == 2: # logistic binary
-        #         pred = [1 if p >= 0.5 else 0 for p in initial_preds]
-        #     else:
-        #         raise Exception("num_class error: input must be integer over 1")
-        # else:
-        #     pred = model.predict(test_vec)
-        # print("="*100)
-        # print(f"Method: {method}")
-        # print(classification_report(args[3], pred))
-        # print("\nconfusion matrix:")
-        # print(confusion_matrix(args[3], pred), "\n")
-        # print("="*100)
+        
         d = {
             "method":method,
         }
         if func.__name__ == "lightgbm_classification" or func.__name__ == "xgboost_classification":
-            d["is_binary"] = True if kwargs["num_class"] <= 2 else False
+            d["is_binary"] = True if kwargs["num_class"] == 2 else False
+        
         test_model(model, test_vec, args[3], **d)
         
-        return model
+        return model, vectorizer, method
         
     return wrapper
         
